5b1a2dc3fd1584c8b3b7bf5710d4a5ae01d9bda3
sid.shi <18262272638@163.com>
2022-08-02 23:00:15 +0800

    add four components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="home">
-    Home
+    <top-view />
+    <sales-view />
+    <bottom-view />
+    <map-view />
   </div>
 </template>
 
 <script>
+import TopView from '../components/TopView'
+import SalesView from '../components/SalesView'
+import BottomView from '../components/BottomView'
+import MapView from '../components/MapView'
 
 export default {
   name: 'HomeView',
   components: {
+    TopView,
+    SalesView,
+    BottomView,
+    MapView
   }
 }
 </script>
+
+<style>
+  .home {
+    width: 100%;
+    height: 100%;
+    padding: 0 20px;
+    background: #eee;
+    box-sizing: border-box;
+  }
+</style>
