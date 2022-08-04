13cb25a55fa93d266de395e5a4813eeae97f4a9b
sid.shi <18262272638@163.com>
2022-08-04 22:00:09 +0800

    add comm style

--- src/components/CommCard/index.vue
+++ src/components/CommCard/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="common-card">
     <div class="title">1</div>
-    <div class="value">2</div>
-    <div class="chart">3</div>
-    <div class="line">4</div>
+    <div class="value"># 222</div>
+    <div class="chart"></div>
+    <div class="line"></div>
     <div class="total">5</div>
   </div>
 </template>
@@ -12,4 +12,27 @@
 export default {}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+  .title {
+    font-size: 12px;
+    color: #999;
+  }
+  .value {
+    font-size: 25px;
+    color: #000;
+    margin-top: 5px;
+    letter-spacing: 1px;
+  }
+  .chart {
+    height: 50px;
+    background: red;
+  }
+  .line {
+    margin: 10px 0;
+    border-top: 1px solid #eee;
+  }
+  .total {
+    font-size: 12px;
+    color: #666;
+  }
+</style>
