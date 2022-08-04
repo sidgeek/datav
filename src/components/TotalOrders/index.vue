ca26c22b2d7ae046f2da4b3dfa1876137d2e6d84
sid.shi <18262272638@163.com>
2022-08-04 22:49:16 +0800

    add echart's picture and replace version from 5 to 4

--- src/components/TotalOrders/index.vue
+++ src/components/TotalOrders/index.vue
@@ -2,7 +2,15 @@
   <comm-card
     title="累计订单量"
     value="$ 320,099,8"
-  />
+  >
+    <template>
+      <div id="total-orders-chart" :style="{ width: '100%', height: '100%' }"/>
+    </template>
+    <template v-slot:footer>
+      <span>昨日订单量</span>
+      <span class="emphasis">￥ 30000</span>
+    </template>
+  </comm-card>
 </template>
 
 <script>
@@ -10,9 +18,39 @@ import CommCardMixinVue from '@/mixins/CommCardMixin.vue'
 
 export default {
   mixins: [CommCardMixinVue],
-  components: {
+  mounted () {
+    const chartDom = document.getElementById('total-orders-chart')
+    const chart = this.$echarts.init(chartDom)
+       chart.setOption({
+        xAxis: {
+          type: 'category',
+          show: false,
+          boundaryGap: false
+        },
+        yAxis: {
+          show: false
+        },
+        series: [{
+          type: 'line',
+          data: [620, 432, 220, 534, 790, 430, 220, 320, 532, 320, 834, 690, 530, 220, 620],
+          areaStyle: {
+            color: 'purple'
+          },
+          lineStyle: {
+            width: 0
+          },
+          itemStyle: {
+            opacity: 0
+          },
+          smooth: true
+        }],
+        grid: {
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0
+        }
+      })
   }
 }
 </script>
-
-<style></style>
