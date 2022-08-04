a3ef402d858a919089f46fff0563d70395da6d5d
sid.shi <18262272638@163.com>
2022-08-04 23:03:03 +0800

    add another table picture

--- src/components/TodayUsers/index.vue
+++ src/components/TodayUsers/index.vue
@@ -2,7 +2,15 @@
   <comm-card
     title="今日交易用户数"
     value="$ 320,099,8"
-  />
+  >
+    <template>
+      <div id="today-users-chart" :style="{ width: '100%', height: '100%' }" />
+    </template>
+    <template v-slot:footer>
+      <span>昨日销售额</span>
+      <span class="emphasis">￥ 30000</span>
+    </template>
+  </comm-card>
 </template>
 
 <script>
@@ -10,7 +18,31 @@ import CommCardMixinVue from '@/mixins/CommCardMixin.vue'
 
 export default {
   mixins: [CommCardMixinVue],
-  components: {
+  mounted () {
+    const chartDom = document.getElementById('today-users-chart')
+    const chart = this.$echarts.init(chartDom)
+    chart.setOption({
+      color: ['#3398DB'],
+      series: [{
+        type: 'bar',
+        data: [410, 82, 200, 334, 390, 330, 220, 150, 82, 200, 134, 290, 330, 150],
+        barWidth: '60%'
+      }],
+      xAxis: {
+        type: 'category',
+        data: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00'],
+        show: false
+      },
+      yAxis: {
+        show: false
+      },
+      grid: {
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0
+      }
+    })
   }
 }
 </script>
