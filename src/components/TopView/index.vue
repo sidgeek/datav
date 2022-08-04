cf80e2b7cc94a2ce1cb660b6c7ee30e2f259dafd
sid.shi <18262272638@163.com>
2022-08-04 08:45:52 +0800

    add comm component

--- src/components/TopView/index.vue
+++ src/components/TopView/index.vue
@@ -2,23 +2,36 @@
   <div class="top-view">
     <el-row :gutter="20">
       <el-col :span="6">
-        <el-card shadow="hover"> 鼠标悬浮时显示 </el-card>
+        <el-card shadow="hover">
+          <total-sales></total-sales>
+        </el-card>
       </el-col>
       <el-col :span="6">
-        <el-card shadow="hover"> 鼠标悬浮时显示 </el-card>
+        <el-card shadow="hover">
+          <total-sales></total-sales>
+        </el-card>
       </el-col>
       <el-col :span="6">
-        <el-card shadow="hover"> 鼠标悬浮时显示 </el-card>
+        <el-card shadow="hover">
+          <total-sales></total-sales>
+        </el-card>
       </el-col>
       <el-col :span="6">
-        <el-card shadow="hover"> 鼠标悬浮时显示 </el-card>
+        <el-card shadow="hover">
+          <total-sales></total-sales>
+        </el-card>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-export default {}
+import TotalSales from '../TotalSales'
+export default {
+  components: {
+    TotalSales
+  }
+}
 </script>
 
 <style></style>
---
+++ src/components/TotalSales/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <comm-card></comm-card>
+</template>
+
+<script>
+import CommCard from '../CommCard'
+
+export default {
+  components: {
+    CommCard
+  }
+}
+</script>
+
+<style></style>
