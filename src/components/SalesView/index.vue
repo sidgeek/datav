80146c82eeb82f3f0bc924d7f6167c01f610a818
sid.shi <18262272638@163.com>
2022-08-05 22:42:59 +0800

    add header

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -10,6 +10,25 @@
             <el-menu-item index="1">销售额</el-menu-item>
             <el-menu-item index="2">访问量</el-menu-item>
           </el-menu>
+          <div class="menu-right">
+            <el-radio-group v-model="radioSelect" size="small">
+              <el-radio-button label="今日" />
+              <el-radio-button label="本周" />
+              <el-radio-button label="本月" />
+              <el-radio-button label="今年" />
+            </el-radio-group>
+            <el-date-picker
+              type="daterange"
+              v-model="date"
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              size="small"
+              unlink-panels
+              :picker-options="pickerOptions"
+              class="sales-view-date-picker"
+            />
+          </div>
         </div>
       </template>
       <template>
@@ -23,7 +42,36 @@
   export default {
     data () {
       return {
-        activeIndex: '1'
+        activeIndex: '1',
+        radioSelect: '今日',
+        date: null,
+        pickerOptions: {
+          shortcuts: [{
+            text: '最近一周',
+            onClick (picker) {
+              const start = new Date()
+              const end = new Date()
+              start.setTime(start.getTime() - 3600 * 24 * 1000 * 7)
+              picker.$emit('pick', [start, end])
+            }
+          }, {
+            text: '最近一个月',
+            onClick (picker) {
+              const start = new Date()
+              const end = new Date()
+              start.setTime(start.getTime() - 3600 * 24 * 1000 * 30)
+              picker.$emit('pick', [start, end])
+            }
+          }, {
+            text: '最近三个月',
+            onClick (picker) {
+              const start = new Date()
+              const end = new Date()
+              start.setTime(start.getTime() - 3600 * 24 * 1000 * 90)
+              picker.$emit('pick', [start, end])
+            }
+          }]
+        }
       }
     },
     methods: {
@@ -37,5 +85,36 @@
 <style lang="scss" scoped>
   .sales-view {
     margin-top: 20px;
+
+    .menu-wrapper {
+      position: relative;
+      display: flex;
+
+      .sales-view-menu {
+        width: 100%;
+        padding-left: 20px;
+
+        .el-menu-item {
+          height: 50px;
+          line-height: 50px;
+          margin: 0 20px;
+        }
+      }
+
+      .menu-right {
+        position: absolute;
+        top: 0;
+        right: 20px;
+        height: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+
+        .sales-view-date-picker {
+          margin-left: 20px;
+        }
+      }
+    }
+
   }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import { Card, Row, Col, Menu, MenuItem, RadioGroup, Radio, DatePicker } from 'element-ui'
+import { Card, Row, Col, Menu, MenuItem, RadioButton, Radio, DatePicker } from 'element-ui'
 
 Vue.use(Card)
 Vue.use(Row)
 Vue.use(Col)
 Vue.use(Menu)
 Vue.use(MenuItem)
-Vue.use(RadioGroup)
+Vue.use(RadioButton)
 Vue.use(Radio)
 Vue.use(DatePicker)
