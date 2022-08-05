c31f3289f73e50c420fbfce300f03c5fa0790b18
sid.shi <18262272638@163.com>
2022-08-05 08:32:33 +0800

    draw custom image

--- src/components/TotalUsers/index.vue
+++ src/components/TotalUsers/index.vue
@@ -4,22 +4,17 @@
     value="$ 320,099,8"
   >
     <template>
-      <div class="compare-wrapper">
-        <div class="compare">
-          <span>日同比</span>
-          <span class="emphasis">7.33%</span>
-          <div class="increase" />
-        </div>
-        <div class="compare">
-          <span>月同比</span>
-          <span class="emphasis">7.33%</span>
-          <div class="decrease" />
-        </div>
-      </div>
+      <div id="total-users-chart" :style="{ width: '100%', height: '100%' }" />
     </template>
     <template v-slot:footer>
-      <span>昨日销售额</span>
-      <span class="emphasis">￥ 30000</span>
+      <div class="total-users-footer">
+        <span>日同比</span>
+        <span class="emphasis">8.73%</span>
+        <div class="increase" />
+        <span class="month">月同比</span>
+        <span class="emphasis">35.91%</span>
+        <div class="decrease" />
+      </div>
     </template>
   </comm-card>
 </template>
@@ -28,8 +23,93 @@
 import CommCardMixinVue from '@/mixins/CommCardMixin.vue'
 
 export default {
-  mixins: [CommCardMixinVue]
+  mixins: [CommCardMixinVue],
+  mounted () {
+    const chartDom = document.getElementById('total-users-chart')
+    const chart = this.$echarts.init(chartDom)
+    chart.setOption({
+      grid: {
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0
+      },
+      xAxis: {
+        type: 'value',
+        show: false
+      },
+      yAxis: {
+        type: 'category',
+        show: false
+      },
+      series: [{
+        type: 'bar',
+        stack: '总量',
+        data: [100],
+        barWidth: 10,
+        itemStyle: {
+          color: '#45c946'
+        }
+      }, {
+        type: 'bar',
+        stack: '总量',
+        data: [250],
+        itemStyle: {
+          color: '#eee'
+        }
+      }, {
+        type: 'custom',
+        stack: '总量',
+        data: [100],
+        renderItem: (params, api) => {
+          const value = api.value(0)
+          const endPoint = api.coord([value, 0])
+
+          return {
+            type: 'group',
+            position: endPoint,
+            children: [{
+              type: 'path',
+              shape: {
+                d: 'M1024 255.996 511.971 767.909 0 255.996 1024 255.996z',
+                x: -5,
+                y: -20,
+                width: 10,
+                height: 10,
+                layout: 'cover'
+              },
+              style: {
+                fill: '#45c946'
+              }
+            }, {
+              type: 'path',
+              shape: {
+                d: 'M0 767.909l512.029-511.913L1024 767.909 0 767.909z',
+                x: -5,
+                y: 10,
+                width: 10,
+                height: 10,
+                layout: 'cover'
+              },
+              style: {
+                fill: '#45c946'
+              }
+            }]
+          }
+        }
+      }]
+    })
+  }
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+  .total-users-footer {
+    display: flex;
+    align-items: center;
+
+    .month {
+      margin-left: 10px;
+    }
+  }
+</style>
