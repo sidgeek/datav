86f0129ef85a0ab6178d8a149622195f19a2dda7
sid.shi <18262272638@163.com>
2022-08-07 14:27:16 +0800

    add bmap init

---
+++ src/views/BMap.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-chart :options="options"></v-chart>
+</template>
+
+<script>
+import 'echarts/extension/bmap/bmap'
+
+export default {
+  data () {
+    return {
+      options: {}
+    }
+  },
+  mounted () {
+    this.options = {
+      bmap: {
+        key: 'dGQGpLSVro3KDObkPGmkLz0t0qScK9c6',
+        center: [104.114129, 37.550339],
+        zoom: 5,
+        roam: false
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
