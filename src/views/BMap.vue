c51a5383602a20a36626adf3c0fbb8150d05c1c9
sid.shi <18262272638@163.com>
2022-08-07 14:44:11 +0800

    add custom style(but seems not work) and title

--- src/views/BMap.vue
+++ src/views/BMap.vue
@@ -12,12 +12,118 @@ export default {
     }
   },
   mounted () {
+    /* eslint-disable */
     this.options = {
+      title: {
+        text: '外卖销售额',
+        subtext: '销售趋势统计',
+        sublink: 'https://www.baidu.com',
+        left: 'center'
+      },
       bmap: {
         key: 'dGQGpLSVro3KDObkPGmkLz0t0qScK9c6',
         center: [104.114129, 37.550339],
         zoom: 5,
-        roam: false
+        roam: true,
+        mapStyle: {
+          styleJson: [{
+            'featureType': 'water',
+            'elementType': 'all',
+            'stylers': {
+              'color': '#d1d1d1'
+            }
+          }, {
+            'featureType': 'land',
+            'elementType': 'all',
+            'stylers': {
+              'color': '#f3f3f3'
+            }
+          }, {
+            'featureType': 'railway',
+            'elementType': 'all',
+            'stylers': {
+              'visibility': 'off'
+            }
+          }, {
+            'featureType': 'highway',
+            'elementType': 'all',
+            'stylers': {
+              'color': '#fdfdfd'
+            }
+          }, {
+            'featureType': 'highway',
+            'elementType': 'labels',
+            'stylers': {
+              'visibility': 'off'
+            }
+          }, {
+            'featureType': 'arterial',
+            'elementType': 'geometry',
+            'stylers': {
+              'color': '#fefefe'
+            }
+          }, {
+            'featureType': 'arterial',
+            'elementType': 'geometry.fill',
+            'stylers': {
+              'color': '#fefefe'
+            }
+          }, {
+            'featureType': 'poi',
+            'elementType': 'all',
+            'stylers': {
+              'visibility': 'off'
+            }
+          }, {
+            'featureType': 'green',
+            'elementType': 'all',
+            'stylers': {
+              'visibility': 'off'
+            }
+          }, {
+            'featureType': 'subway',
+            'elementType': 'all',
+            'stylers': {
+              'visibility': 'off'
+            }
+          }, {
+            'featureType': 'manmade',
+            'elementType': 'all',
+            'stylers': {
+              'color': '#d1d1d1'
+            }
+          }, {
+            'featureType': 'local',
+            'elementType': 'all',
+            'stylers': {
+              'color': '#d1d1d1'
+            }
+          }, {
+            'featureType': 'arterial',
+            'elementType': 'labels',
+            'stylers': {
+              'visibility': 'off'
+            }
+          }, {
+            'featureType': 'boundary',
+            'elementType': 'all',
+            'stylers': {
+              'color': '#fefefe'
+            }
+          }, {
+            'featureType': 'building',
+            'elementType': 'all',
+            'stylers': {
+              'color': '#d1d1d1'
+            }
+          }, {
+            'featureType': 'label',
+            'elementType': 'labels.text.fill',
+            'stylers': {
+              'color': '#999999'
+            }
+          }]
+        }
       }
     }
   }
