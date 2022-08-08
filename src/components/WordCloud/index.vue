c4c1f5e799c5fcdefaf7b04253a55347b8a04306
sid.shi <18262272638@163.com>
2022-08-08 08:43:58 +0800

    add word cloud

--- src/components/WordCloud/index.vue
+++ src/components/WordCloud/index.vue
@@ -1,10 +1,27 @@
 <template>
-  <div> 222 </div>
+  <ve-wordcloud :data="chartData" height="100%" :settings="chartSettings" />
 </template>
 
 <script>
 
 export default {
+  data () {
+    return {
+      chartData: {
+        columns: ['name', 'value'],
+        rows: [{
+          name: '测试',
+          value: 100
+        }, {
+          name: '行为',
+          value: 70
+        }]
+      },
+      chartSettings: {
+        color: ['rgba(97,216,0,.7)', 'rgba(204,178,26,.7)', 'rgba(245,166,35,.7)', 'rgba(156,13,113,.7)']
+      }
+    }
+  }
 }
 </script>
 
